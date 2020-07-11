0276dd53f0eae5d3105f1c97874ee3cabdf3c42e
mountainguy76935 <stoll33david@gmail.com>
2020-07-11 13:37:10 -0600

    security

--- server.js
+++ server.js
@@ -6,7 +6,15 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000
 
-console.log('logs', process.env.REACT_APP_USER_LOCATION)
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+      return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+  }
+
+app.use(requireHTTPS)
 
 app.use(express.static(path.join(__dirname, 'build')))
 
