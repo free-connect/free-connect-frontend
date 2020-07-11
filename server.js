875ab163ca30afea1e9e74307a10a5a1a39a01b6
mountainguy76935 <stoll33david@gmail.com>
2020-07-11 11:45:59 -0600

    did some stuff

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000
 
+console.log('logs', process.env.REACT_APP_USER_LOCATION)
+
 app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('*', (req, res, next) => {
