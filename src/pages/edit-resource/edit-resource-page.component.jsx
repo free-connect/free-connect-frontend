3b1581523e80cebcc82b3474b905b3913d70414f
mountainguy76935 <stoll33david@gmail.com>
2020-07-13 17:47:16 -0600

    fixed bug with resizing

--- src/pages/edit-resource/edit-resource-page.component.jsx
+++ src/pages/edit-resource/edit-resource-page.component.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import AddResource from '../../components/resources/add-resource-form/add-resource.component';
 import { withRouter } from 'react-router-dom';
+import { Loading } from '../../components/loading-icon/loading.component';
 
 const EditResourcePage = (props) => {
+    const [authed, setAuthed] = React.useState(false)
+
+    React.useEffect(() => {
+        if (props.location.state) {
+            if (props.location.state.edit) {
+                setAuthed(true)
+                return;
+            } else {
+                props.history.push('/')
+            }
+        } else {
+            props.history.push('/')
+        }
+    }, [])
+
     return (
+        <React.Fragment>
+        {authed ? 
         <div>
             <AddResource {...props} />
-        </div>
+        </div> : 
+        <Loading />}
+        </React.Fragment>
     )
 }
 
