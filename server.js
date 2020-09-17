256bd8daf79b344f7a4a205cea0f431f77b53588
mountainguy76935 <stoll33david@gmail.com>
2020-09-17 11:14:08 -0600

    forgot to switch the server back on! updated

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ const port = process.env.PORT || 3000
 
 //uncomment when pushing to github
 
-// function requireHTTPS(req, res, next) {
-//     // The 'x-forwarded-proto' check is for Heroku
-//     if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
-//       return res.redirect('https://' + req.get('host') + req.url);
-//     }
-//     next();
-//   }
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+      return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+  }
 
-// app.use(requireHTTPS)
+app.use(requireHTTPS)
 
 // app.use((req, res, next) => {
 //     res.set({
